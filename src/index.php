0d1254e8c07f7978fad8ea51bfbe641c4b47dc21
shoma.sato <kd1355892@st.kobedenshi.ac.jp>
2025-04-22 14:28:49 +0900

    test1

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <link rel="stylesheet" href="css/index.css?<?php echo date('YmdHis'); ?>"/>
+    <meta charset="utf-8">
+    <title>Welcome!</title>
+  </head>
+  <body>
+    <article>
+      <section id="section1">
+        <h2>こんにちは</h2>
+      </section>
+      <section id="section2">
+        <p>さようなら</p>
+      </section>  
+    </article>
+  </body>
+</html>
