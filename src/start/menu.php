dc1c5d6f034ba79be81de7446d8a3c87f68199d7
shoma.sato <kd1355892@st.kobedenshi.ac.jp>
2025-06-04 10:33:39 +0900

    mysqlを除いた共有用のフォルダー

---
+++ src/start/menu.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/startmenu.css?<?php echo date('YmdHis'); ?>"/>
+    <title>タイトル画面</title>
+</head>
+<body>
+    <h1>タイトル画面</h1>
+</body>
+</html>
