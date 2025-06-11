00e93d9512c655fa88ad823ab4036d8398483be7
shoma.sato <kd1355892@st.kobedenshi.ac.jp>
2025-06-11 12:02:50 +0900

    テスト

---
+++ src/game/question.php
@@ -0,0 +1,10 @@
+<pre>
+    <code>
+    行番号のつぎに内容を動的に出力する(csv形式)
+    1| print('Hello, world !')
+    2| x = 10
+    3|
+    4| print(x*3)
+    </code>
+</pre>
+</div>
--- src/start/menu.php
+++ src/start/menu.php
@@ -7,7 +7,22 @@
     <title>タイトル画面</title>
 </head>
 <body>
-    <h1>タイトル画面</h1>
-    
+    <div class="title_text">
+        <h1>Error is Our Friend</h1>
+    </div>
+    <div class="start_button_area">
+        <form action="../game/game.php" method="GET">
+            <div class="level_radio">
+                <input type="radio" name="level" value="easy" class="level" checked>Easy
+                <input type="radio" name="level" value="normal" class="level">Normal
+            </div>
+            <input type="submit" class="start_button" value="Start">
+        </form>
+    </div>
+    <div class="score_button_area">
+        <form action="../score/list.php" method="GET">
+            <input type="submit" class="score_button" value="Score">
+        </form>
+    </div>
 </body>
 </html>
